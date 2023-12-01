4afcce335032eb466102c1fa9061fb914157e0fd
Francisco Matheus de Sousa França <RH.MatheusFranca@opovo.local>
2023-12-01 15:24:24 -0300

    Criando um array de objetos para o UseState

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
-function App() {
+import { useState } from "react"
 
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id:1,
+      text:"Criar funcionalidade x no sistema",
+      category: "Trabalho",
+      isCompleted: false
+    },
+    {
+      id:2,
+      text:"Criar HUB de Dados",
+      category: "Trabalho",
+      isCompleted: false
+    },
+    {
+      id:3,
+      text:"Faculade",
+      category: "Faculdade",
+      isCompleted: false
+    }
+  ])
   return (
     <>
-
+      <div className="app">
+        
+      </div>
     </>
   )
 }
